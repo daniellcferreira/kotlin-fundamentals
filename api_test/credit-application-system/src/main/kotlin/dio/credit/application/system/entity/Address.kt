797e9da0cbed560fc36c7df8da5c698b43d30e94
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-11-17 16:15:05 -0300

    update lesson

--- api_test/credit-application-system/src/main/kotlin/dio/credit/application/system/entity/Address.kt
+++ api_test/credit-application-system/src/main/kotlin/dio/credit/application/system/entity/Address.kt
@@ -1,9 +1,10 @@
 package dio.credit.application.system.entity
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
 
 @Embeddable
 data class Address(
-    @Column(nullable = false) var zipCode: String = "",
-    @Column(nullable = false) var street: String = ""
+  @Column(nullable = false) var zipCode: String = "",
+  @Column(nullable = false) var street: String = ""
 )
--- api_test/credit-application-system/src/main/kotlin/dio/credit/application/system/entity/Credit.kt
+++ api_test/credit-application-system/src/main/kotlin/dio/credit/application/system/entity/Credit.kt
@@ -1,20 +1,18 @@
 package dio.credit.application.system.entity
 
+import jakarta.persistence.*
 import dio.credit.application.system.enummeration.Status
-import dio.credit.application.system.entity.Custumer
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.UUID
-import jakarta.persistence.*
 
 @Entity
-@Table(name = "Credit")
-data class Credit(
-    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
-    @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
-    @Column(nullable = false) val dayFirstInstallment: LocalDate,
-    @Column(nullable = false) val numberOfInstallments: Int = 0,
-    @Enumerated val status: Status = Status.IN_PROGRESS,
-    @ManyToOne val custumer: Custumer? = null,
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
-)
+@Table(name = "Credito")
+data class Credit (
+  @Column(nullable = false, unique = true) var creditCode: UUID = UUID.randomUUID(),
+  @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
+  @Column(nullable = false) val dayFirstInstallment: LocalDate,
+  @Column(nullable = false) val numberOfInstallments: Int = 0,
+  @Enumerated val status: Status = Status.IN_PROGRESS,
+  @ManyToOne var customer: Customer? = null,
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null)
